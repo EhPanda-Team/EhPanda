1ab688b247c1f888a0817dd104136b2598c2e66f
たつぞう <kendellcarol@gmail.com>
2020-10-28 22:32:28 +0800

    最初のコミット、Hello World~

---
+++ EhPandaApp.swift
@@ -0,0 +1,20 @@
+//
+//  EhPandaApp.swift
+//  EhPanda
+//
+//  Created by 荒木辰造 on R 2/10/28.
+//
+
+import SwiftUI
+
+@main
+struct EhPandaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
