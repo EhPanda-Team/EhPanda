e47548ab6ea4f0cd79aa094b1bb750e0eb430cbb
たつぞう <kendellcarol@gmail.com>
2022-01-10 10:19:22 +0900

    feat: Add `Private` category support
    # Conflicts:
    #	EhPanda/Models/EhSetting.swift

--- EhPanda/Models/Models.swift
+++ EhPanda/Models/Models.swift
@@ -289,6 +289,10 @@ extension Category {
             return 128
         case .misc:
             return 1
+        case .private:
+            let message = "Category `Private` shouldn't be used in filters!"
+            Logger.error(message)
+            fatalError(message)
         }
     }
 }
@@ -317,6 +321,9 @@ extension Language {
 enum Category: String, Codable, CaseIterable, Identifiable {
     var id: String { rawValue }
 
+    static let allFavoritesCases: [Category] = [.misc] + allCases.dropLast(2)
+    static let allFiltersCases: [Category] = allCases.dropLast()
+
     case doujinshi = "Doujinshi"
     case manga = "Manga"
     case artistCG = "Artist CG"
@@ -327,6 +334,7 @@ enum Category: String, Codable, CaseIterable, Identifiable {
     case cosplay = "Cosplay"
     case asianPorn = "Asian Porn"
     case misc = "Misc"
+    case `private` = "Private"
 }
 
 enum TagCategory: String, Codable, CaseIterable {
