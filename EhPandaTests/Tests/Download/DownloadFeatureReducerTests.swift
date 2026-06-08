49ebe436d1537b6a09e2cf3b567fef426ff65a4a
Chihchy <chihchy.h@proton.me>
2026-06-08 22:02:26 +0800

    Delete test shell

--- EhPandaTests/Tests/Download/DownloadFeatureReducerTests.swift
+++
@@ -1,7 +0,0 @@
-//
-//  DownloadFeatureReducerTests.swift
-//  EhPandaTests
-//
-//  Tests have been split into separate files by feature area.
-//  See the other files in this directory for the individual test suites.
-//
