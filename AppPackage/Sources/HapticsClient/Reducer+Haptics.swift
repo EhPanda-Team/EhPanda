cbc463fb72b17834bc6e8ab66473165d8baaefbf
Chihchy <chihchy.h@proton.me>
2026-07-02 07:36:19 +0800

    Delete SwiftUINavigationExt and direct dep

--- AppPackage/Sources/HapticsClient/Reducer+Haptics.swift
+++ AppPackage/Sources/HapticsClient/Reducer+Haptics.swift
@@ -1,9 +1,8 @@
 import SwiftUI
 import ComposableArchitecture
-import SwiftUINavigationExt
 
 extension Reducer {
-    public func haptics<Enum: Sendable, Case: Sendable>(
+    public func haptics<Enum, Case>(
         unwrapping enum: @escaping (State) -> Enum?,
         case caseKeyPath: CaseKeyPath<Enum, Case>,
         hapticsClient: HapticsClient,
@@ -14,15 +13,16 @@ extension Reducer {
         }
     }
 
-    private func onBecomeNonNil<Enum: Sendable, Case: Sendable>(
+    private func onBecomeNonNil<Enum, Case>(
         unwrapping enum: @escaping (State) -> Enum?,
         case caseKeyPath: CaseKeyPath<Enum, Case>,
         perform additionalEffects: @escaping (inout State, Action) -> Effect<Action>
     ) -> some Reducer<State, Action> {
         Reduce { state, action in
-            let previousCase = Binding.constant(`enum`(state)).case(caseKeyPath).wrappedValue
+            let casePath = AnyCasePath(caseKeyPath)
+            let previousCase = `enum`(state).flatMap(casePath.extract(from:))
             let effects = _reduce(into: &state, action: action)
-            let currentCase = Binding.constant(`enum`(state)).case(caseKeyPath).wrappedValue
+            let currentCase = `enum`(state).flatMap(casePath.extract(from:))
 
             return previousCase == nil && currentCase != nil
                 ? .merge(effects, additionalEffects(&state, action))
