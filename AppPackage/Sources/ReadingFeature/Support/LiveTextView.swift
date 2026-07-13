917cc1fa96f4fc8d6bb9de0d17c98795f0e00962
Chihchy <chihchy.h@proton.me>
2026-07-13 13:42:27 +0800

    refactor(05-07): preserve live text geometry without reader

--- AppPackage/Sources/ReadingFeature/Support/LiveTextView.swift
+++ AppPackage/Sources/ReadingFeature/Support/LiveTextView.swift
@@ -5,6 +5,7 @@ struct LiveTextView: View {
     private let liveTextGroups: [LiveTextGroup]
     private let focusedLiveTextGroup: LiveTextGroup?
     private let tapAction: (LiveTextGroup) -> Void
+    @State private var size: CGSize = .zero
 
     init(
         liveTextGroups: [LiveTextGroup],
@@ -17,84 +18,84 @@ struct LiveTextView: View {
     }
 
     var body: some View {
-        GeometryReader { proxy in
-            let size = proxy.size
-            let width = size.width
-            let height = size.height
-            ZStack {
-                Canvas { context, _ in
-                    context.fill(
-                        Path(CGRect(x: 0, y: 0, width: width, height: height)),
-                        with: .color(.black.opacity(0.1))
-                    )
-                    let tuples: [(UUID, Path)] = liveTextGroups
-                        .flatMap { group in
-                            group.blocks.map { block in
-                                (group.id, block)
-                            }
-                        }
-                        .map { (id, block) in
-                            let expandingSize = 4.0
-                            let bounds = block.bounds
-                            let topLeft = bounds.topLeft * size
-                            let rect = CGRect(
-                                x: 0, y: 0,
-                                width: bounds.getWidth(size) + expandingSize * 2,
-                                height: bounds.getHeight(size) + expandingSize * 2)
+        ZStack {
+            Canvas { context, canvasSize in
+                context.fill(
+                    Path(CGRect(origin: .zero, size: canvasSize)),
+                    with: .color(.black.opacity(0.1))
+                )
+                guard size != .zero else { return }
 
-                            let path = Path(roundedRect: rect, cornerRadius: bounds.getHeight(size) / 5)
-                                .applying(CGAffineTransform(rotationAngle: block.bounds.getRadian(size)))
-                                .offsetBy(dx: topLeft.x - expandingSize, dy: topLeft.y - expandingSize)
-                            return (id, path)
+                let tuples: [(UUID, Path)] = liveTextGroups
+                    .flatMap { group in
+                        group.blocks.map { block in
+                            (group.id, block)
                         }
-                    context.withCGContext { cgContext in
-                        tuples.forEach { (_, path) in
-                            cgContext.setFillColor(.init(red: 255, green: 255, blue: 255, alpha: 1))
-                            cgContext.setShadow(
-                                offset: .zero, blur: 15,
-                                color: .init(red: 0, green: 0, blue: 0, alpha: 0.3)
+                    }
+                    .map { (id, block) in
+                        let expandingSize = 4.0
+                        let bounds = block.bounds
+                        let topLeft = bounds.topLeft * size
+                        let rect = CGRect(
+                            x: 0, y: 0,
+                            width: bounds.getWidth(size) + expandingSize * 2,
+                            height: bounds.getHeight(size) + expandingSize * 2)
+
+                        let path = Path(roundedRect: rect, cornerRadius: bounds.getHeight(size) / 5)
+                            .applying(CGAffineTransform(rotationAngle: block.bounds.getRadian(size)))
+                            .offsetBy(dx: topLeft.x - expandingSize, dy: topLeft.y - expandingSize)
+                        return (id, path)
+                    }
+                context.withCGContext { cgContext in
+                    tuples.forEach { (_, path) in
+                        cgContext.setFillColor(.init(red: 255, green: 255, blue: 255, alpha: 1))
+                        cgContext.setShadow(
+                            offset: .zero, blur: 15,
+                            color: .init(red: 0, green: 0, blue: 0, alpha: 0.3)
+                        )
+                        cgContext.addPath(path.cgPath)
+                        cgContext.drawPath(using: .fill)
+                    }
+                }
+                context.blendMode = .destinationOut
+                tuples.forEach { (_, path) in
+                    context.fill(path, with: .color(.red))
+                    context.stroke(path, with: .color(.red))
+                }
+
+                if let focusedLiveTextGroup = focusedLiveTextGroup {
+                    context.blendMode = .copy
+                    tuples.forEach { (groupUUID, path) in
+                        if groupUUID == focusedLiveTextGroup.id {
+                            context.stroke(
+                                path, with: .color(.accentColor.opacity(0.6)),
+                                style: .init(lineWidth: 10)
                             )
-                            cgContext.addPath(path.cgPath)
-                            cgContext.drawPath(using: .fill)
                         }
                     }
                     context.blendMode = .destinationOut
-                    tuples.forEach { (_, path) in
-                        context.fill(path, with: .color(.red))
-                        context.stroke(path, with: .color(.red))
-                    }
-
-                    if let focusedLiveTextGroup = focusedLiveTextGroup {
-                        context.blendMode = .copy
-                        tuples.forEach { (groupUUID, path) in
-                            if groupUUID == focusedLiveTextGroup.id {
-                                context.stroke(
-                                    path, with: .color(.accentColor.opacity(0.6)),
-                                    style: .init(lineWidth: 10)
-                                )
-                            }
-                        }
-                        context.blendMode = .destinationOut
-                        tuples.forEach { (groupUUID, path) in
-                            if groupUUID == focusedLiveTextGroup.id {
-                                context.fill(path, with: .color(.accentColor))
-                            }
+                    tuples.forEach { (groupUUID, path) in
+                        if groupUUID == focusedLiveTextGroup.id {
+                            context.fill(path, with: .color(.accentColor))
                         }
                     }
                 }
+            }
 
+            if size != .zero {
                 ForEach(liveTextGroups) { textGroup in
                     HighlightView(text: textGroup.text) {
                         tapAction(textGroup)
                     }
-                    .frame(width: textGroup.width * width, height: textGroup.height * height)
+                    .frame(width: textGroup.width * size.width, height: textGroup.height * size.height)
                     .position(
-                        x: (textGroup.minX + textGroup.width / 2) * width,
-                        y: (textGroup.minY + textGroup.height / 2) * height
+                        x: (textGroup.minX + textGroup.width / 2) * size.width,
+                        y: (textGroup.minY + textGroup.height / 2) * size.height
                     )
                 }
             }
         }
+        .onGeometryChange(for: CGSize.self) { $0.size } action: { size = $0 }
     }
 }
 
